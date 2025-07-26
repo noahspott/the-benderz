f136b075a437e2e53b13947537d33bfcec65e318
noahspott <noahspottdev@gmail.com>
2025-07-26 15:05:13 -0300

    feat: add meta titles and descriptions to all pages via Seo component

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -2,3 +2,4 @@ export { default as FontPreloader } from "./FontPreloader.astro";
 export { default as OrganizationSchema } from "./OrganizationSchema.astro";
 export { default as OpenGraph } from "./OpenGraph.astro";
 export { default as Favicon } from "./Favicon.astro";
+export { default as Seo } from "./Seo.astro";
