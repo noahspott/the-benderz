60e34cdd5eb9b40e5a320c4dbcbb1b0b146b413a
noahspott <noahspottdev@gmail.com>
2026-01-14 14:37:24 -0300

    feat: make event list sortable by date

--- sanity.config.ts
+++ sanity.config.ts
@@ -10,7 +10,29 @@ export default defineConfig({
   projectId: "4r9ag2ef",
   dataset: "production",
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool({
+      structure: (S) =>
+        S.list()
+          .title("Content")
+          .items(
+            schemaTypes.map((type) => {
+              if (type.name === "event") {
+                return S.listItem()
+                  .title(type.title || type.name)
+                  .schemaType(type.name)
+                  .child(
+                    S.documentTypeList(type.name).defaultOrdering([
+                      { field: "date", direction: "desc" },
+                    ])
+                  );
+              }
+              return S.documentTypeListItem(type.name);
+            })
+          ),
+    }),
+    visionTool(),
+  ],
 
   schema: {
     types: schemaTypes as SchemaTypeDefinition[],
